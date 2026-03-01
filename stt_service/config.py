8b62472fd67c6fa5409491285f56ec12802c0a3c
Yosef shoval <ys0542522504@gmail.com>
2026-03-01 13:59:53 +0200

    convert datetime object to str in metadata json. add kafka config

--- stt_service/config.py
+++ stt_service/config.py
@@ -1,3 +1,4 @@
+import os
 from os import getenv
 import logging
 
@@ -5,8 +6,10 @@ import logging
 class Config:
     audio_files_folder = os.getenv('podcasts_folder')
 
-    producer_config = {}
+    kafka_topic = os.getenv('KAFKA_TOPIC')
+    producer_config = {
+        'bootstrap.servers' : os.getenv('KAFKA_URL')
+    }
 
     logger = logging.getLogger('metadata extractor')
     logging.basicConfig(level=logging.INFO)
-
--- stt_service/metadata_extractor.py
+++ stt_service/metadata_extractor.py
@@ -9,10 +9,12 @@ def get_metadata(file : Path):
     logger.info(f'getting_metadata for file: {file}')
     return {
         'file_name' : file.name,
-        'created_at' : datetime.datetime.fromtimestamp(file.stat().st_ctime),
-        'modified_at' : datetime.datetime.fromtimestamp(file.stat().st_mtime),
-        'size' : stat().st_size,
-        'suffix' : file.suffix,
+        'created_at' : str(datetime.datetime.fromtimestamp(file.stat().st_ctime)),
+        'modified_at' : str(datetime.datetime.fromtimestamp(file.stat().st_mtime)),
+        'size' : file.stat().st_size,
+        'suffix' : file.suffix
     }
 
 
+x = get_metadata(Path(r"C:\podcasts\download (8).wav"))
+print(x)
