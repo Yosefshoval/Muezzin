d7af1562b36d41cc2067c040934bfbac321a73e0
Yosef shoval <ys0542522504@gmail.com>
2026-03-05 10:03:09 +0200

    add env's in config for kafka and logger, add kafka consumer

--- stt_service/config.py
+++ stt_service/config.py
@@ -5,3 +5,14 @@ class Config:
     elastic_url = os.getenv('ELASTIC_URL')
     elastic_index = os.getenv('ELASTIC_INDEX')
 
+    kafka_url = os.getenv('KAFKA_URL')
+    kafka_topic = os.getenv('KAFKA_TOPIC')
+
+    consumer_config = {
+        "bootstrap.servers" : kafka_url,
+        "group.id" : "uuid_service",
+        "auto.offset.reset" : "earliest"
+    }
+
+    logger = Logger.get_logger(name=' stt_service ', es_host=elastic_url)
+    logger.info('logger created')
