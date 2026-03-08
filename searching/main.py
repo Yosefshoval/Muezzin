69a1a34d2fa1a76e3de2ecec0a098b8f3ba7b0a5
Yosef shoval <ys0542522504@gmail.com>
2026-03-08 10:14:47 +0200

    write basic server, add config class

---
+++ searching/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, HTTPException
+import uvicorn
+import dal
+import elastic
+from config import SearchingConfig
+
+
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return {
+        "message" : "FastAPI server running and healthy!"
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app="main:app",
+        host="0.0.0.0",
+        port=8080,
+        reload=True
+    )
