5094f7399da4377228ca8f7a23d25bb8883d58ce
Yosef shoval <ys0542522504@gmail.com>
2026-03-09 10:19:25 +0200

    add result handler for clean response, get_all endpoint

--- searching/dal.py
+++ searching/dal.py
@@ -1,7 +1,23 @@
 from elastic import Elastic
 
-def top_5_terrorists(client: Elastic):
-    query = {"match_all" : {}}
-    result = client.exec_query(query)
+
+def result_handler(result: dict):
+    clean_result = []
+    if result is not None:
+        for item in result:
+            clean_result.append(item['_source'])
+        return clean_result
     return result
 
+
+def get_all(client: Elastic):
+    query = { "query" : { "match_all" : { } } }
+    result = client.exec_query(query)
+    return result_handler(result)
+
+
+
+def top_5_terrorists(client: Elastic):
+    query = {}
+    result = client.exec_query(query)
+    return result_handler(result)
--- searching/main.py
+++ searching/main.py
@@ -16,6 +16,17 @@ def home():
         "message" : "FastAPI server running and healthy!"
     }
 
+
+@app.get('/query/all-data')
+def get_all_data():
+    try:
+        result = dal.get_all(elastic_client)
+        return result
+    except Exception as e:
+        logger.error(f'{type(e)} : {e}')
+        raise HTTPException(500, f'{type(e)} : {e}')
+
+
 @app.get('/query/top-5-terrorists')
 def query_1():
     try:
